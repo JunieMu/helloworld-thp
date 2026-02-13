e29eafdac9c2163a0b53d27e6bdd6c985177d9fc
Joyce Ma <joycema@Joyces-MacBook-Pro-5.local>
2026-02-13 15:00:39 -0500

    testing google oauth2

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,27 @@
-import { createMiddlewareClient } from '@supabase/ssr'
+import { createServerClient } from '@supabase/ssr'
 import { NextResponse } from 'next/server'
 
 import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
-  const supabase = createMiddlewareClient({ req, res })
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return req.cookies.get(name)?.value
+        },
+        set(name: string, value: string, options: any) {
+          req.cookies.set({ name, value, ...options })
+        },
+        remove(name: string, options: any) {
+          req.cookies.set({ name, value: '', ...options })
+        },
+      },
+    }
+  )
 
   await supabase.auth.getSession()
 
