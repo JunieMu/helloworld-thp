12f6bd4f968406a9f29af8d6032a76af188e78e5
Joyce Ma <joycema@Joyces-MacBook-Pro-5.local>
2026-02-13 15:19:55 -0500

    testing google oauth8

--- middleware.ts
+++ middleware.ts
@@ -14,10 +14,10 @@ export async function middleware(req: NextRequest) {
           return req.cookies.get(name)?.value
         },
         set(name: string, value: string, options: any) {
-          req.cookies.set({ name, value, ...options })
+          res.cookies.set(name, value, options)
         },
         remove(name: string, options: any) {
-          req.cookies.set({ name, value: '', ...options })
+          res.cookies.set(name, '', options)
         },
       },
     }
