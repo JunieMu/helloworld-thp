65f810593496cb3b586c58fce8b31ff8e574ee1b
Joyce Ma <joycema@Joyces-MacBook-Pro-5.local>
2026-02-13 15:06:41 -0500

    testing google oauth5

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -2,19 +2,17 @@ import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export function createClient() {
-  const cookieStore = cookies()
-
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
     {
       cookies: {
         get(name: string) {
-          return cookieStore.get(name)?.value
+          return cookies().get(name)?.value
         },
         set(name: string, value: string, options: any) {
           try {
-            cookieStore.set({ name, value, ...options })
+            cookies().set({ name, value, ...options })
           } catch (error) {
             // The `cookies().set()` method can only be called in a Server Component or Route Handler.
             // This error is typically caused by an attempt to set a cookie from a Client Component
@@ -24,7 +22,7 @@ export function createClient() {
         },
         remove(name: string, options: any) {
           try {
-            cookieStore.set({ name, value: '', ...options })
+            cookies().set({ name, value: '', ...options })
           } catch (error) {
             // The `cookies().set()` method can only be called in a Server Component or Route Handler.
             // This error is typically caused by an attempt to set a cookie from a Client Component
